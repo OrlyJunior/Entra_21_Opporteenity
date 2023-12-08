8d1e229b7964b1a6257d524908f19e591772175f
Orly Antunes Borges Junior <orly.junior@proway.treina>
2023-12-08 15:28:22 -0300

    Primeira versão de adicionar serviços, não salvando em BD e sem Edit, Delete e Details.

---
+++ ProjetoE21/Controllers/ServiçosController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjetoE21.Models;
+
+namespace ProjetoE21.Controllers
+{
+    public class ServiçosController : Controller
+    {
+        static List<Servico> servicos = new();
+        public IActionResult Index()
+        {
+            return View(servicos);
+        }
+
+        [HttpGet]
+        public IActionResult Adicionar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Adicionar(Servico servico)
+        {
+            Empresa empresa = new();
+            empresa.Nome = "Empresa inventada";
+            empresa.Id = 1;
+
+            servico.EmpresaS = new();
+
+            servico.EmpresaS.Id = empresa.Id;
+            servico.EmpresaS.Nome = empresa.Nome;
+
+            servico.Dia = $"{servico.Horario.Day}/{servico.Horario.Month}/{servico.Horario.Year}";
+            servico.Hora = $"{servico.Horario.TimeOfDay}";
+
+            servico.Id = servicos.Count + 1;
+
+            servicos.Add(servico);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ProjetoE21/Models/Servico.cs
@@ -0,0 +1,13 @@
+﻿namespace ProjetoE21.Models
+{
+    public class Servico
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public Empresa EmpresaS { get; set; }
+        public decimal Pagamento { get; set; }
+        public DateTime Horario { get; set; }
+        public string Dia { get; set; }
+        public string Hora { get; set; }
+    }
+}
