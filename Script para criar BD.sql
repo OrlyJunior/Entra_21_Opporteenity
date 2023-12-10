fab24b9a8ab7f6a7f9f7ebc6f0085227911f3704
unknown <orlyjunior21@gmail.com>
2023-12-09 22:55:55 -0300

    Nova versão do script para a criação do BD adicionada.

--- Script para criar BD.sql
+++ Script para criar BD.sql
@@ -4,7 +4,26 @@ create table tb_servicos(
 id int auto_increment primary key,
 descricao varchar(40) not null,
 contratanteNome varchar(40) not null,
+estado varchar(40) not null,
+cidade varchar(40) not null,
+numero int not null,
+bairro varchar(40) not null,
+rua varchar(40) not null,
 data datetime not null,
 dia varchar(20) not null,
 hora varchar(20) not null,
 valor decimal not null);
+
+create table tb_empregos(
+id int auto_increment primary key,
+descricao varchar(40) not null,
+empresaNome varchar(40) not null,
+horaInicio varchar(30) not null,
+horaTermino varchar(30) not null,
+estado varchar(40) not null,
+cidade varchar(40) not null,
+numero int not null,
+bairro varchar(40) not null,
+rua varchar(40) not null,
+salario decimal not null,
+diasTrabalhados int not null);
