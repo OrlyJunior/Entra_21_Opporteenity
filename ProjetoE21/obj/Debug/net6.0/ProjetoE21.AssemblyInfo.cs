9e82fd3e26d57caa65a275ec28aedf7727db8683
Orly Antunes Borges Junior <orly.junior@proway.treina>
2024-02-09 14:11:51 -0300

    Botão do Whattsapp funcional.

--- ProjetoE21/obj/Debug/net6.0/ProjetoE21.AssemblyInfo.cs
+++ ProjetoE21/obj/Debug/net6.0/ProjetoE21.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoE21")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56afd3c5da63ab404d6a6446dff0f5c2fc4114cb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58581ab0eec56c0dffaf257d3c0c0971f3d4a1d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoE21")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoE21")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
