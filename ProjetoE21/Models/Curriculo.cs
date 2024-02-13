9b0bf286f6955ece77301143697146c73a3d3252
PatrickMaiak <patrickmaiakremer@gmail.com>
2024-02-13 10:39:00 -0300

    implementação do novo curriculo no perfil de jovens

--- ProjetoE21/Models/Curriculo.cs
+++ ProjetoE21/Models/Curriculo.cs
@@ -2,6 +2,29 @@
 {
     public class Curriculo
     {
+
+        // public string Nome { get; set; }
+        // public string Telefone { get; set; }
+        // public string Email { get; set; }
+        public string PerfilProfissional { get; set; }
+        public string Cidade { get; set; }
+        public string InicioEscola { get; set; }
+        public string Idioma1 { get; set; }
+        public string EscolaNome { get; set; }
+        public string NivelIdioma1 { get; set; }
+        public string EscolaCidade { get; set; }
+        public string Idioma2 { get; set; }
+        public string Ensino { get; set; }
+        public string NivelIdioma2 { get; set; }
+        public string Status { get; set; }
+        public string Idioma3 { get; set; }
+        public string NivelIdioma3 { get; set; }
+        // public string Cursos { get; set; }
+        public string Outros1 { get; set; }
+        public string Outros2 { get; set; }
+        public string Outros3 { get; set; }
+        public string Outros4 { get; set; }
+
         public int Id { get; set; }
         public string Nome { get; set; }
         public Local Local { get; set; }
--- ProjetoE21/obj/Debug/net6.0/ProjetoE21.AssemblyInfo.cs
+++ ProjetoE21/obj/Debug/net6.0/ProjetoE21.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoE21")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94cc12278841785bec3892076d3c399fb897e3d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+146bfbd8efd6a77994b8745a317113433d0691fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoE21")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoE21")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
