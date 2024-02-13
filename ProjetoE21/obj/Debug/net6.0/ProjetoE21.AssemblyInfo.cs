a5d5629ae57dccbfa3cf5e8d2f1f4b1d347457f5
PatrickMaiak <patrickmaiakremer@gmail.com>
2024-02-13 12:52:30 -0300

    implementação do dropdown no login e preparação da div para receber as informaçoes do canva

--- ProjetoE21/obj/Debug/net6.0/ProjetoE21.AssemblyInfo.cs
+++ ProjetoE21/obj/Debug/net6.0/ProjetoE21.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoE21")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+146bfbd8efd6a77994b8745a317113433d0691fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b0bf286f6955ece77301143697146c73a3d3252")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoE21")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoE21")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
