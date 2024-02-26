57e4d8c294752b4600e66a0ae3e76e23234b1661
PatrickMaiak <patrickmaiakremer@gmail.com>
2024-02-26 17:10:03 -0300

    fetch manual entre o front de 26/02 e o filtro de distancia

--- ProjetoE21/obj/Debug/net6.0/ProjetoE21.AssemblyInfo.cs
+++ ProjetoE21/obj/Debug/net6.0/ProjetoE21.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoE21")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8353d469b19216577c41e3e399611e3dc0f8e9d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59c7c56452f68eef43a6f4dc8cd82f4d056e364a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoE21")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoE21")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
